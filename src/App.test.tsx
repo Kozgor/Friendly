843431d99aba8bf1a13161006b964560ac5430c2
Ihor <57453880+Kozgor@users.noreply.github.com>
2023-08-09 18:14:26 +0300

    Added Jest. Added test precommit with Husky

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Welcome!', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const password = screen.getByText(/Password/i);
+
+  expect(password).toBeInTheDocument();
 });
