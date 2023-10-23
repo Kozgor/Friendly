67e1e2ee528ede88302045e008178329d3d5b6d9
Ihor <57453880+Kozgor@users.noreply.github.com>
2023-10-23 10:20:07 +0300

    feat: Participants component fetch all users

---
+++ src/components/DefaultBoard/DeafaultColumns.ts
@@ -0,0 +1,26 @@
+export const INITIAL_COLUMNS = [
+  {
+      columnId: 'start',
+      columnTitle: 'START',
+      columnSubtitle: '',
+      columnAvatar: '',
+      columnStyle: '',
+      columnCards: []
+  },
+  {
+      columnId: 'stop',
+      columnTitle: 'STOP',
+      columnSubtitle: '',
+      columnAvatar: '',
+      columnStyle: '',
+      columnCards: []
+  },
+  {
+      columnId: 'continue',
+      columnTitle: 'CONTINUE',
+      columnSubtitle: '',
+      columnAvatar: '',
+      columnStyle: '',
+      columnCards: []
+  }
+];
