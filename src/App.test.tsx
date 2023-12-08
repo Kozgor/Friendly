ce2a9e696d8093caea12bea32c08f5bbfb77809f
Yura935 <boyko.yuriy16@gmail.com>
2023-12-08 11:02:47 +0200

    Feat: ssfr-15-colors-configuration-in-joy: Fixing tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -29,6 +29,22 @@ describe('App component', () => {
   let component: RenderResult;
   const navigate = jest.fn();
 
+  beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(),
+        removeListener: jest.fn(),
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn()
+      }))
+    });
+  });
+
   beforeEach(() => {
     localStorage.setItem('fullName', 'Admin');
     localStorage.setItem('token', 'testToken');
